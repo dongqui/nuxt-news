0a0c87fd01a1e4a586baa9fae32f72563db234ce
Unknown <kimwinner90@gmail.com>
2020-06-04 18:45:40 +0900

    add nav component

---
+++ components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <h1>News</h1>
+    <ul>
+      <li>Login</li>
+      <li>Register</li>
+      <li>Categorie</li>      
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+nav {
+  height: 60px;
+}
+
+h1 {
+  line-height: 60px;
+  margin-left: 20px;
+  display: inline-block;
+}
+
+ul {
+  float: right;
+  height: 60px;
+  margin-right: 20px;
+}
+
+li {
+  display: inline;
+  margin: 0 20px;
+  line-height: 60px;
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    <NavBar/>
     <nuxt/>
   </div>
 </template>
+
+<script>
+  import NavBar from '../components/NavBar';
+  export default {
+    components: {
+      NavBar
+    }
+  }
+</script>
 
 <style>
 html {
