64f0d4a0d6e6d801ffb24313b3d6971f9c774d7d
Unknown <kimwinner90@gmail.com>
2020-06-05 16:19:37 +0900

    set headline

--- store/headLines.js
+++ store/headLines.js
@@ -1,18 +1,23 @@
-export const SET_HEADLINES = 'FETCH_HEADLINES';
+export const SET_HEADLINES = 'SET_HEADLINES';
+export const SET_HEADLINE = 'SET_HEADLINE'
 
 export const state = () => ({
     headLines: [],
+    headLine: null,
 });
 
 export const mutations = {
-  SET_HEADLINES(state, params) {
-    state.headLines = params;
+  [SET_HEADLINES](state, headLines) {
+    state.headLines = headLines;
+  },
+  [SET_HEADLINE](state, headLine) {
+    state.headLine = headLine;
   }
 };
 
 export const actions = {
   async fetchHeadLines({ commit }) {
     const res = await this.$axios.$get('https://newsapi.org/v2/top-headlines?country=us');
-    commit('SET_HEADLINES', res.articles);
+    commit(SET_HEADLINES, res.articles);
   }
 };
