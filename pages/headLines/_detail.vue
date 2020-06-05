f503e95746ffaf819e6bd49f874deb4a7c9621b2
Unknown <kimwinner90@gmail.com>
2020-06-05 16:19:09 +0900

    set headline

---
+++ pages/headLines/_detail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{headLine.title}}</h2>
+    <p>{{headLine.content}}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+  computed: {
+    ...mapState({
+      headLine: state => state.headLines.headLine,
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
